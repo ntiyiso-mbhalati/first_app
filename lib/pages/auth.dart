97a59f560af951faa756e39bc43664bc22d6c6c5
NTIYISO MBHALATI <ntiyisombhalati@NTIYISOs-MacBook-Pro.local>
2018-10-22 22:32:10 +0200

    swith widget

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -8,6 +8,8 @@ class _AuthPageState extends State<AuthPage> {
   String _usernameValue;
   String _passwordValue;
 
+  bool _acceptTerms = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,7 +37,15 @@ class _AuthPageState extends State<AuthPage> {
               });
             },
           ),
-          Switch(),
+          SwitchListTile(
+            value: _acceptTerms,
+            onChanged: (bool value) {
+              setState(() {
+                _acceptTerms = value;
+              });
+            },
+            title: Text('Accept Terms'),
+          ),
           SizedBox(
             height: 10.0,
           ),
